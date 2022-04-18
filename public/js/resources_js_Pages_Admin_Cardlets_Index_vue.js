90ede2bd4756823c976862c3b352c8c7cb210710
Didier Senou <didiersenou94@gmail.com>
2022-04-18 16:06:53 +0100

    no cardlet found ui implementation

--- public/js/resources_js_Pages_Admin_Cardlets_Index_vue.js
+++ public/js/resources_js_Pages_Admin_Cardlets_Index_vue.js
@@ -892,6 +892,7 @@ var _hoisted_11 = /*#__PURE__*/_withScopeId(function () {
 });
 
 var _hoisted_12 = {
+  key: 0,
   "class": "bg-white divide-y divide-gray-200"
 };
 var _hoisted_13 = {
@@ -920,29 +921,41 @@ var _hoisted_20 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNo
 
 var _hoisted_21 = {
   key: 1,
+  "class": "work text-lg w-full text-center font-medium py-10 p-2",
+  style: {
+    "display": "table-caption",
+    "caption-side": "bottom"
+  }
+};
+var _hoisted_22 = {
+  key: 1,
   "class": ""
 };
 
-var _hoisted_22 = /*#__PURE__*/_withScopeId(function () {
+var _hoisted_23 = /*#__PURE__*/_withScopeId(function () {
   return /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, null, -1
   /* HOISTED */
   );
 });
 
-var _hoisted_23 = {
+var _hoisted_24 = {
   "class": "flex items-center pb-1 justify-between"
 };
-var _hoisted_24 = {
+var _hoisted_25 = {
   "class": "font-medium text-base"
 };
-var _hoisted_25 = {
+var _hoisted_26 = {
   "class": "font-semibold"
 };
-var _hoisted_26 = {
+var _hoisted_27 = {
   "class": "flex items-center justify-between"
 };
-var _hoisted_27 = {
+var _hoisted_28 = {
   "class": "text-gray-700 text-sm"
+};
+var _hoisted_29 = {
+  key: 1,
+  "class": "work text-lg w-full text-center font-medium py-10 p-2"
 };
 function render(_ctx, _cache, $props, $setup, $data, $options) {
   var _component_GoBack = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("GoBack");
@@ -1048,7 +1061,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
         _: 1
         /* STABLE */
 
-      })]), $setup.tablet ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_9, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("table", _hoisted_10, [_hoisted_11, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("tbody", _hoisted_12, [((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($props.cardlets, function (cardlet) {
+      })]), $setup.tablet ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_9, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("table", _hoisted_10, [_hoisted_11, $props.cardlets.length ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("tbody", _hoisted_12, [((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($props.cardlets, function (cardlet) {
         return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("tr", {
           key: cardlet.id,
           "class": "py-2"
@@ -1060,7 +1073,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
         /* TEXT */
         ), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("td", _hoisted_16, " ₦" + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(cardlet.rate), 1
         /* TEXT */
-        ), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" <td\r\n                                    class=\"\r\n                                                    px-6\r\n                                                    py-4\r\n                                                    whitespace-nowrap\r\n                                                    text-sm text-gray-500\r\n                                                    capitalize\r\n                                                \"\r\n                            >\r\n                                {{ REPLACE_UNDERSCORE(cardlet.condition) }}\r\n                            </td> "), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("td", _hoisted_17, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_q_badge, {
+        ), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" <td\r\n                                            class=\"\r\n                                                            px-6\r\n                                                            py-4\r\n                                                            whitespace-nowrap\r\n                                                            text-sm text-gray-500\r\n                                                            capitalize\r\n                                                        \"\r\n                                    >\r\n                                        {{ REPLACE_UNDERSCORE(cardlet.condition) }}\r\n                                    </td> "), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("td", _hoisted_17, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_q_badge, {
           color: $setup.statusColor(cardlet.status)
         }, {
           "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
@@ -1090,18 +1103,20 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
         , ["href"])])]);
       }), 128
       /* KEYED_FRAGMENT */
-      ))])])])) : ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_21, [((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($props.cardlets, function (cardlet) {
+      ))])) : ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_21, " No Giftcards found "))])])) : ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_22, [$props.cardlets.length ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        key: 0
+      }, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($props.cardlets, function (cardlet) {
         return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createBlock)(_component_inertia_link, {
           key: cardlet.id,
           href: _ctx.route('cardlet.show', cardlet.id),
           "class": "border block work m-4 bg-white shadow p-3 rounded"
         }, {
           "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
-            return [_hoisted_22, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_23, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_24, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(cardlet.name), 1
+            return [_hoisted_23, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_24, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_25, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(cardlet.name), 1
             /* TEXT */
-            ), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_25, "$" + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(cardlet.amount), 1
+            ), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_26, "$" + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(cardlet.amount), 1
             /* TEXT */
-            )]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_26, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_27, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(cardlet.created_at.slice(0, 10)), 1
+            )]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_27, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_28, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(cardlet.created_at.slice(0, 10)), 1
             /* TEXT */
             ), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_q_badge, {
               color: $setup.statusColor(cardlet.status)
@@ -1126,7 +1141,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
         , ["href"]);
       }), 128
       /* KEYED_FRAGMENT */
-      ))]))])];
+      )) : ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_29, " No Giftcards found "))]))])];
     }),
     _: 1
     /* STABLE */
--- resources/js/Pages/Admin/Cardlets/Index.vue
+++ resources/js/Pages/Admin/Cardlets/Index.vue
@@ -100,62 +100,68 @@
                             </th>
                         </tr>
                     </thead>
-                    <tbody class="bg-white divide-y divide-gray-200">
-                        <tr
-                            v-for="cardlet in cardlets"
-                            :key="cardlet.id"
-                            class="py-2">
-                            <td
-                                class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 capitalize">
-                                {{ cardlet.name }}
-                            </td>
-                            <td
-                                class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 capitalize">
-                                {{ cardlet.type }}
-                            </td>
-                            <td
-                                class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 font-medium capitalize">
-                                ${{ cardlet.amount }}
-                            </td>
-                            <td
-                                class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 capitalize">
-                                &#8358;{{ cardlet.rate }}
-                            </td>
-                            <!-- <td
-                                    class="
-                                                    px-6
-                                                    py-4
-                                                    whitespace-nowrap
-                                                    text-sm text-gray-500
-                                                    capitalize
-                                                "
-                            >
-                                {{ REPLACE_UNDERSCORE(cardlet.condition) }}
-                            </td> -->
-                            <td
-                                class="px-2 py-4 whitespace-nowrap text-sm text-gray-500 capitalize">
-                                <q-badge :color="statusColor(cardlet.status)">
-                                    {{ cardlet.status }}
-                                </q-badge>
-                            </td>
-                            <td
-                                class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 font-medium capitalize">
-                                &#8358;{{ +cardlet.amount * +cardlet.rate }}
-                            </td>
-                            <td
-                                class="px-6 py-4 whitespace-nowrap text-right text-sm flex gap-4 font-medium">
-                                <inertia-link
-                                    :href="route('cardlet.show', cardlet.id)"
-                                    class="flex-0 items-center px-4 py-1 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                                    View Card
-                                </inertia-link>
-                            </td>
-                        </tr>
-                    </tbody>
+                        <template v-if="cardlets.length"> 
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                <tr
+                                    v-for="cardlet in cardlets"
+                                    :key="cardlet.id"
+                                    class="py-2">
+                                    <td
+                                        class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 capitalize">
+                                        {{ cardlet.name }}
+                                    </td>
+                                    <td
+                                        class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 capitalize">
+                                        {{ cardlet.type }}
+                                    </td>
+                                    <td
+                                        class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 font-medium capitalize">
+                                        ${{ cardlet.amount }}
+                                    </td>
+                                    <td
+                                        class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 capitalize">
+                                        &#8358;{{ cardlet.rate }}
+                                    </td>
+                                    <!-- <td
+                                            class="
+                                                            px-6
+                                                            py-4
+                                                            whitespace-nowrap
+                                                            text-sm text-gray-500
+                                                            capitalize
+                                                        "
+                                    >
+                                        {{ REPLACE_UNDERSCORE(cardlet.condition) }}
+                                    </td> -->
+                                    <td
+                                        class="px-2 py-4 whitespace-nowrap text-sm text-gray-500 capitalize">
+                                        <q-badge :color="statusColor(cardlet.status)">
+                                            {{ cardlet.status }}
+                                        </q-badge>
+                                    </td>
+                                    <td
+                                        class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 font-medium capitalize">
+                                        &#8358;{{ +cardlet.amount * +cardlet.rate }}
+                                    </td>
+                                    <td
+                                        class="px-6 py-4 whitespace-nowrap text-right text-sm flex gap-4 font-medium">
+                                        <inertia-link
+                                            :href="route('cardlet.show', cardlet.id)"
+                                            class="flex-0 items-center px-4 py-1 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                                            View Card
+                                        </inertia-link>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </template>
+                        <div v-else class="work text-lg w-full text-center font-medium py-10 p-2" style="display: table-caption; caption-side: bottom;">
+                            No Giftcards found
+                        </div>
                 </table>
             </div>
             <div v-else class="">
-                <inertia-link
+                <template v-if="cardlets.length">
+                    <inertia-link
                     v-for="cardlet in cardlets"
                     :key="cardlet.id"
                     :href="route('cardlet.show', cardlet.id)"
@@ -176,6 +182,13 @@
                         </div>
                     </div>
                 </inertia-link>
+                </template>
+                <template v-else>
+                    <div class="work text-lg w-full text-center font-medium py-10 p-2" >
+                            No Giftcards found
+                        </div>
+                </template>
+                
             </div>
         </div>
     </admin-layout>
